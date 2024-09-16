b51c8b14326830b6c32763e162ad3ee87533ab06
Muadh <muadhok@gmail.com>
2024-09-16 14:31:32 +0300

    Corrected spelling mistake

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,8 +1,8 @@
 #Ask user to input their current age
-current_age = int(input("how old areyou?"))
+current_age = int(input("how old are you?"))
 
 #Calculate the age in the year 2050
 future_age = current_age + 27 #since 2050 is 27 years from 2023
-
+32
 #print result
 print("in 2050, you will be", future_age, "years old.")
