6c496bdfb62da10441e7da51cc945591a59b16f6
Muadh <muadhok@gmail.com>
2024-09-16 14:01:52 +0300

    Add simple interest calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#Define the variables
+principle = 1000
+rate = 0.05
+time = 3
+
+#calculate simple interest
+interest = principle * rate * time
+
+#Print result
+print("The simple interest is:", interest)
