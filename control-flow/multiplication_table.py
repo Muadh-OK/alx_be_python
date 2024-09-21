f166441350518fcb8d01b770cc2fa0cf55cfd80a
Muadh <muadhok@gmail.com>
2024-09-21 12:47:28 +0300

    Add multiplication table program

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,4 @@ number = int(input("Enter a number to see its multiplication table:"))
 
 for i in range(1, 11): # Loop from 1 to 10
     result = number * i # calculate the product
-    print (number, "*", i, "=", result)
+    print(f"{number} * {i} = {result}")  # Using f-strings to format output
